57c1040506e6407b0cc6704de6b6b6b3a47898ba
sinaalipour77@hotmail.com <123sinaSINA>
2022-01-22 16:53:21 +0330

    #

---
+++ SinaShop.WebApp/Middlewares/RedirectToValidLangMiddleware.cs
@@ -0,0 +1,16 @@
+﻿namespace SinaShop.WebApp.Middlewares
+{
+    public class RedirectToValidLangMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RedirectToValidLangMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+        }
+    }
+}
