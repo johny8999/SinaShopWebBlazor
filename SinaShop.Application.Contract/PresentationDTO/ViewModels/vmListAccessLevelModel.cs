3c254834bc69b8cd06999f6e136aed0e8ce6b97a
johny jackson <sinaalipour77@hotmail.com>
2022-06-02 21:54:28 +0430

    6/2/2022-22:00

--- SinaShop.Application.Contract/PresentationDTO/ViewModels/vmListAccessLevelModel.cs
+++ SinaShop.Application.Contract/PresentationDTO/ViewModels/vmListAccessLevelModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SinaShop.Application.Contract.PresentationDTO.ViewModels
 {
     public class vmListAccessLevelModel
-    {
+{
+        public string Id { get; set; }
+
+        [Display(Name = nameof(Name))]
+        public string Name { get; set; }
+
+        [Display(Name = nameof(CountUser))]
+        public int CountUser { get; set; }
     }
 }
--- SinaShop.WebApp/Common/ExMethod/KendoEx.cs
+++ SinaShop.WebApp/Common/ExMethod/KendoEx.cs
@@ -1,18 +1,34 @@
 ﻿using FrameWork.Application.Services.Localizer;
 using Kendo.Mvc.UI.Fluent;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SinaShop.WebApp.Common.ExMethod
 {
     public static class KendoEx
     {
-        public static GridBuilder<T> DefaultSettings<T>(this GridBuilder<T> Builder, ILocalizer Localizer) where T : class
+        public static GridBuilder<T> DefaultSettings<T>(this GridBuilder<T> builder, ILocalizer localizer) where T : class
         {
-            return null;
+            return builder.Pageable(a =>
+            {
+                a.Messages(msg =>
+                {
+                    msg.Display(localizer["TelerikPageToFrom"]);
+                    msg.Empty(localizer["ItemNotFound"]);
+                    msg.ItemsPerPage(localizer["TelerikItemsPerPage"]);
+                    msg.Of(localizer["TelerikOf"]);
+                    msg.MorePages(localizer["TelerikMorePages"]);
+                    msg.Refresh(localizer["Refresh"]);
+                    msg.Previous(localizer["Previous"]);
+                    msg.Next(localizer["Next"]);
+                    msg.First(localizer["First"]);
+                    msg.Page(localizer["Page"]);
+                    msg.Last(localizer["Last"]);
+                });
+                a.AlwaysVisible(true);
+                a.ButtonCount(5);
+                a.Input(false);
+                a.PreviousNext(true);
+                a.Responsive(true);
+            });
         }
     }
 }
