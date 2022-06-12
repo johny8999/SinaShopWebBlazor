6800396f3ae22deedb5d25a0218f6ec166a68326
johny jackson <sinaalipour77@hotmail.com>
2022-06-12 18:41:53 +0430

    #

---
+++ FrameWork/Utility/Paging/OutPagingData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWork.Utility.Paging
+{
+    public class OutPagingData
+    {
+        public OutPagingData()
+        {
+                
+        }
+        public OutPagingData(int Take)
+        {
+            CountAllItem = 0;
+            CountAllPAge = 1;
+            Page = 1;
+            this.Take = Take;
+            Skip = 0;
+            StartPage = 1;
+            EndtPage = 1;
+        }
+        public long CountAllItem { get; set; }
+        public int CountAllPAge { get; set; }
+        public int Page { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+        public int StartPage { get; set; }
+        public int EndtPage { get; set; }
+
+    }
+}
